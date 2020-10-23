aa02a18b2f9c55037cdcc0c218c2f9d89207eb62
LavanyaSubbiah <73289796+LavanyaSubbiah@users.noreply.github.com>
2020-10-23 18:27:15 +0530

    Update DivisibleBy.java

--- src/DivisibleBy.java
+++ src/DivisibleBy.java
@@ -21,7 +21,7 @@ public class DivisibleBy {
 		// TODO Auto-generated method stub
 		int sum=0;
 		for(int i=0;i<=value;i++) {
-		if((i%3==0)&&(i%5==0))
+		if((i%3==0)||(i%5==0))
 		{
 			sum=sum+i;
 		}
