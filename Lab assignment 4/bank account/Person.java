69de19de8e7c65ba5ea7bcd997e2a5a95c2fd0ed
LavanyaSubbiah <73289796+LavanyaSubbiah@users.noreply.github.com>
2020-11-02 20:56:03 +0530

    Add files via upload

---
+++ Lab assignment 4/bank account/Person.java
@@ -0,0 +1,48 @@
+/**
+ * author Lavanya Subbiah
+ * date 29/10/2020
+ * desc Person class contains name and age of a person
+ */
+package LabAssignment4;
+
+public class Person {
+
+	/**
+	 * declaring the variables
+	 */
+          private String name;
+          private float age;
+          /**
+           * parameterised constructor
+           * @param name
+           * @param age
+           */
+
+          public Person(String name,float age) {
+        	  //super();
+        	  this.name=name;
+        	  this.age=age;
+          }
+          /**
+           * getters and setters
+           * @return
+           */
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public float getAge() {
+			return age;
+		}
+
+		public void setAge(float age) {
+			this.age = age;
+		}
+          
+	
+
+}
